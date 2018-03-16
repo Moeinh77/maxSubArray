9aaf18703e57dbfe5a86132e6909dccc3d9f408a
Moeinh77 <moeinhasani77@gmail.com>
2018-03-16 12:10:13 +0330

    calculates the keft and right sum Value array replaced with sth better!

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,36 @@ public class Main {
     private static int a = 0, b = 0;
 
     public static void main(String[] args) {
+
         int arr[] = {-12, 1, 4, 6, 3, 5, 2, 9, -2, -7};
         for (int anArr : arr) {
             System.out.print(" ," + anArr);
         }
-        System.out.println("\n");
 
-        findSubArr(arr);
+        int n=arr.length;
 
-        System.out.println();
-        for(int x=a;x<=b;x++){
-            System.out.print(" ,"+arr[x]);
-        }
+        System.out.println("\n"+"mid is "+n/2+"\n");
+
+
+        System.out.println("Crossing sum is "+findSubArr(arr,0,n));
+        System.out.println("Right sum is "+findSubArr(arr,n/2,n));
+        System.out.println("Left sum is "+findSubArr(arr,0,n/2));
+
+//        System.out.println();
+//        for(int x=a;x<=b;x++){
+//            System.out.print(" ,"+arr[x]);
+//        }
     }
 
-    private static void findSubArr(int arr[]) {
+    private static int findSubArr(int arr[],int s,int e) {
 
-        int n = arr.length;
-
-        int[] value = new int[n];
-
-        int av = n / 2;
+        int av = (e-s)/2+s;
         int i = 0;
         int sum = 0;
         int sum_R=Integer.MIN_VALUE,sum_L=Integer.MIN_VALUE;
-        i = av + 1;
+        i = av ;
 
-        while (i < n) {
+        while (i < e) {
             sum += arr[i];
             if(sum>sum_R) {
                 sum_R = sum;
@@ -41,7 +44,7 @@ public class Main {
         sum = 0;
         i = av - 1;
 
-        while (i >= 0) {
+        while (i >= s) {
             sum += arr[i];
             if(sum>sum_L) {
                 sum_L= sum;
@@ -50,6 +53,7 @@ public class Main {
             i--;
         }
 
-        System.out.println(a + " " + b);
+
+        return sum_R+sum_L;
     }
 }
